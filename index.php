7251568a66ef670dfb4248c3d942291013517d61
root <root@master-1.example.com>
2020-08-22 05:44:40 -0400

    build2

--- index.php
+++ index.php
@@ -3,6 +3,6 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>This is s2i image deployed by command Rahul</p>'; ?> 
+ <?php echo '<p>This is s2i image deployed by command Changed</p>'; ?> 
  </body>
 </html>
